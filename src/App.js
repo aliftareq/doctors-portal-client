07d7bb51ae77ca1cd3d4da63b2bfd53cdb8a33ca
aliftareq <aliftareq@gmail.com>
2022-11-12 00:10:57 +0600

    Layout setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hwllow world</h1>
+      <h1 className="text-3xl font-bold text-green-400 underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </div>
   );
 }
