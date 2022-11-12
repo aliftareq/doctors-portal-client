41d37b93cfb9b9f557374d0453fb7876f68fc0e1
aliftareq <aliftareq@gmail.com>
2022-11-12 17:10:16 +0600

    footer modified

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-neutral text-neutral-content">
-            <div>
-                <span className="footer-title">Services</span>
-                <Link to='/' className="link link-hover">Branding</Link>
-                <Link to='/' className="link link-hover">Design</Link>
-                <Link to='/' className="link link-hover">Marketing</Link>
-                <Link to='/' className="link link-hover">Advertisement</Link>
+        <footer className=' footerBgImg text-gray-800'>
+            <div className="footer p-10">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Branding</Link>
+                    <Link to='/' className="link link-hover">Design</Link>
+                    <Link to='/' className="link link-hover">Marketing</Link>
+                    <Link to='/' className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to='/' className="link link-hover">About us</Link>
+                    <Link to='/' className="link link-hover">Contact</Link>
+                    <Link to='/' className="link link-hover">Jobs</Link>
+                    <Link to='/' className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to='/' className="link link-hover">Terms of use</Link>
+                    <Link to='/' className="link link-hover">Privacy policy</Link>
+                    <Link to='/' className="link link-hover">Cookie policy</Link>
+                </div>
             </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <Link to='/' className="link link-hover">About us</Link>
-                <Link to='/' className="link link-hover">Contact</Link>
-                <Link to='/' className="link link-hover">Jobs</Link>
-                <Link to='/' className="link link-hover">Press kit</Link>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <Link to='/' className="link link-hover">Terms of use</Link>
-                <Link to='/' className="link link-hover">Privacy policy</Link>
-                <Link to='/' className="link link-hover">Cookie policy</Link>
+            <div className='text-center mt-3'>
+                <p>Copyright © 2022 - All right reserved by Doctor's Potal</p>
             </div>
         </footer>
     );
